6d3a30156ffdf3e6206fdb882b3407e6660adead
kazunorifujita <upstair37@gmail.com>
2024-04-21 21:23:49 +0900

    css変更

--- src/public/template/pc/App.tsx
+++ src/public/template/pc/App.tsx
@@ -1,10 +1,7 @@
 import "../common/css/App.css";
-import CenterSection from "./CenterSection";
 import Footer from "./Footer";
 import Header from "./Header";
-import LeftSection from "./LeftSection";
 import MainContent from "./MainContent";
-import RightSection from "./RightSection";
 
 export const App = () => {
   return (
