4666e17aa3e938de640bac10fb8b0a99db9f6ff6
kazunorifujita <upstair37@gmail.com>
2024-04-26 22:28:51 +0900

    LeftSection

--- src/public/template/pc/LeftSection.tsx
+++ src/public/template/pc/LeftSection.tsx
@@ -4,6 +4,9 @@ const LeftSection: React.FC = () => {
   return (
     <div className="LeftSection">
       <h5>Left Section</h5>
+      <li>Item1</li>
+      <li>Item2</li>
+      <li>Item3</li>
     </div>
   );
 };
