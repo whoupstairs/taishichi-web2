ba5ca4d5d38f61239ef64f39819589ffff80c58b
kazunorifujita <upstair37@gmail.com>
2024-04-27 23:21:43 +0900

    greybox

---
+++ src/public/template/pc/GreyBox.tsx
@@ -0,0 +1,12 @@
+// GreyBox.tsx
+import React from "react";
+
+const GreyBox: React.FC = () => {
+  return (
+    <div style={{ width: "100%", height: "500px", backgroundColor: "#f3f3f3" }}>
+      Grey Box
+    </div>
+  );
+};
+
+export default GreyBox;
--- src/public/template/pc/RightSection.tsx
+++ src/public/template/pc/RightSection.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import GreyBox from "./GreyBox";
 
 const RightSection: React.FC = () => {
   return (
     <div className="RightSection">
       <h5>Right Section</h5>
+      <GreyBox />
     </div>
   );
 };
