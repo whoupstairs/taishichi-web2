8cc15d09fb353bc2fc279e693145fe7909825d89
kazunorifujita <upstair37@gmail.com>
2024-04-28 22:24:16 +0900

    css化

--- src/public/template/pc/GreyBox.tsx
+++ src/public/template/pc/GreyBox.tsx
@@ -2,11 +2,7 @@
 import React from "react";
 
 const GreyBox: React.FC = () => {
-  return (
-    <div style={{ width: "100%", height: "500px", backgroundColor: "#f3f3f3" }}>
-      Grey Box
-    </div>
-  );
+  return <div className="GreyBox">Grey Box</div>;
 };
 
 export default GreyBox;
